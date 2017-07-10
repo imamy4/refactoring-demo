220a742e86d571e8911506c4d715fe08af8c5f98
imamy4 <rais_93@mail.ru>
2017-07-10 16:40:38 +0300

    first commit

---
+++ src/test/java/intro/CustomerTest.java
@@ -0,0 +1,55 @@
+package intro;
+
+import com.scrumtrek.simplestore.Customer;
+import com.scrumtrek.simplestore.Movie;
+import com.scrumtrek.simplestore.PriceCodes;
+import com.scrumtrek.simplestore.Rental;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by imamy on 10.07.2017.
+ */
+public class CustomerTest {
+    @Test
+    public void itShouldBLABLAWhenBLALA(){
+        //region Fixture | Arrange | Given
+        Movie movCinderella = new Movie("Cinderella", PriceCodes.Childrens);
+        Movie movStarWars = new Movie("Star Wars", PriceCodes.Regular);
+        Movie movGladiator = new Movie("Gladiator", PriceCodes.NewRelease);
+
+        Customer sut = new Customer("Mickey Mouse");
+
+        // Create rentals
+        Rental rental1 = new Rental(movCinderella, 1);
+        Rental rental2 = new Rental(movStarWars, 1);
+        Rental rental3 = new Rental(movGladiator, 1);
+        Rental rental4 = new Rental(movCinderella, 5);
+        Rental rental5 = new Rental(movStarWars, 5);
+        Rental rental6 = new Rental(movGladiator, 5);
+
+        sut.addRental(rental1);
+        sut.addRental(rental2);
+        sut.addRental(rental3);
+        sut.addRental(rental4);
+        sut.addRental(rental5);
+        sut.addRental(rental6);
+        //endregion
+
+        //region Act | When
+
+        String stat1 = sut.Statement();
+
+        //endregion
+
+        //region Assert | Then
+
+        Assert.assertTrue(stat1.contains(movCinderella.getTitle()) &&
+                stat1.contains(movStarWars.getTitle()) &&
+                stat1.contains(movGladiator.getTitle()));
+
+        //endregion
+    }
+}
---
+++ src/test/java/intro/MovieTest.java
@@ -0,0 +1,57 @@
+package intro;
+
+import org.junit.Test;
+import com.scrumtrek.simplestore.*;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * SUT: system under test
+ * Test Case: class
+ * Test, Test scenario: method
+ */
+public class MovieTest {
+    @Test
+    public void itShouldGetCorrectTitleAndPriceCodeWhenCreateMovie() { //BDD: Domain Lang
+        //region Fixture | Arrange | Given
+        final String title = "Matrix";
+        final PriceCodes priceCode = PriceCodes.Regular;
+
+        Movie sut = null;
+        //endregion
+
+        //region Act | When
+        sut = new Movie(title, priceCode);
+        //endregion
+
+        //region Assert | Then
+        /**
+         * AssertionError | Failure
+         */
+        assertEquals(title, sut.getTitle());
+        assertEquals(priceCode, sut.getPriceCode());
+        //endregion
+    }
+
+    @Test
+    public void itShouldGetCorrectPriceCodeWhenSetPriceCode() { //BDD: Domain Lang
+        //region Fixture | Arrange | Given
+        final String title = "Matrix";
+        final PriceCodes priceCode = PriceCodes.NewRelease;
+        final PriceCodes newCode = PriceCodes.Regular;
+
+        Movie sut = new Movie(title, priceCode);
+        //endregion
+
+        //region Act | When
+        sut.setPriceCode(newCode);
+        //endregion
+
+        //region Assert | Then
+        /**
+         * AssertionError | Failure
+         */
+        assertEquals(newCode, sut.getPriceCode());
+        //endregion
+    }
+}
